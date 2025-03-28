0932c96ca650528205d2e502043db4759790434f
flicksolutions <sebastian@flick.solutions>
2025-03-28 17:11:48 +0100

    use d3 for coordinates

--- src/routes/timeline/Timeline.svelte
+++ src/routes/timeline/Timeline.svelte
@@ -5,6 +5,25 @@
 	let { width, height, topic: currenttopic, year: currentyear } = $props();
 
 	const colors = ['#FF5733', '#33FF57', '#3357FF'];
+	const PADDING = 20;
+
+	const years = [
+		...new Set(Object.values(data).flatMap((content) => content.map((datapoint) => datapoint.Jahr)))
+	].sort((a, b) => a - b);
+	const x = d3
+		.scaleLinear()
+		.domain([years[0], years[years.length - 1]])
+		.range([PADDING, width - PADDING]);
+	const y = d3
+		.scaleBand()
+		.domain(['Up', 'Heilmann', 'Brunnen', 'Neuhaus', 'Down'])
+		.range([0, height])
+		.padding(0.2);
+
+	// const lineGenerator = d3
+	// 	.line()
+	// 	.x((d) => x(d.Jahr))
+	// 	.y((d) => (d.Ort === 'Biel' ? y('Up') : y('Down')));
 
 	let svgElement;
 </script>
@@ -13,12 +32,17 @@
 	{#each Object.entries(data) as [topic, content], i}
 		<g>
 			<foreignObject x="1" y={i * 100 + 1} width="100" height="100">
-				<div xmlns="http://www.w3.org/1999/xhtml" class="text-xs text-gray-500">{topic}</div>
+				<div class="text-xs text-gray-500">{topic}</div>
 			</foreignObject>
 			{#each content as datapoint, j}
 				<!-- svelte-ignore a11y_consider_explicit_label -->
 				<a href="/timeline/{topic}/{datapoint.Jahr}">
-					<circle cx={j + 1} cy={datapoint.Ort === 'Biel' ? i + 2 : i - 1} r="5" fill={colors[i]} />
+					<circle
+						cx={x(datapoint.Jahr)}
+						cy={datapoint.Ort === 'Biel' ? y(topic) : y(i === 0 ? 'Up' : 'Down')}
+						r="5"
+						fill={colors[i]}
+					/>
 				</a>
 			{/each}
 		</g>
