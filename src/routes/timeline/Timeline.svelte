9de91d56c22c28ccc2ec271076dee0370c5b4610
flicksolutions <sebastian@flick.solutions>
2025-03-31 17:25:36 +0200

    finalize timeline

--- src/routes/timeline/Timeline.svelte
+++ src/routes/timeline/Timeline.svelte
@@ -1,34 +1,43 @@
 <script lang="ts">
-	import { scaleLinear, scaleBand, line } from 'd3';
+	import { scaleLinear, scalePoint, line } from 'd3';
 	import data from '$lib/data/main.json';
 
 	let { width, height, topic: currenttopic, year: currentyear } = $props();
 
 	const colors = ['#FF5733', '#33FF57', '#3357FF'];
 	const PADDING = 20;
+	const GUTTER = 220;
 
 	const years = [
 		...new Set(Object.values(data).flatMap((content) => content.map((datapoint) => datapoint.Jahr)))
 	].sort((a, b) => a - b);
-	const x = $derived(
+	let x = $derived(
 		scaleLinear()
 			.domain([years[0], years[years.length - 1]])
-			.range([PADDING, width - PADDING])
+			.range([PADDING + GUTTER, width - PADDING])
 	);
-	const y = $derived(
-		scaleBand()
+	let y = $derived(
+		scalePoint()
 			.domain(['Up', 'Heilmann', 'Brunnen', 'Neuhaus', 'Down'])
 			.range([0, height])
 			.padding(0.2)
-			.round(true)
 	);
 	let svgElement;
 </script>
 
 <svg {width} {height} bind:this={svgElement}>
+	{#each ['Up', 'Down'] as wrapper}
+		<foreignObject x="1" y={(y(wrapper) || 0) - 11} width="100" height="22">
+			<div class={['text-sm', 'text-gray-500']}>Andernorts</div>
+		</foreignObject>
+	{/each}
+	<foreignObject x="1" y={(y('Brunnen') || 0) - 11} width="100" height="22">
+		<div class={['text-sm', 'text-gray-500']}>Biel</div>
+	</foreignObject>
+
 	{#each Object.entries(data) as [topic, content], i}
 		<g>
-			<foreignObject x="1" y={y(topic)} width="100" height="100">
+			<foreignObject x="70" y={(y(topic) || 0) - 11} width="100" height="22">
 				<div class={['text-sm', topic === currenttopic ? 'text-primary-500' : 'text-gray-500']}>
 					{topic}
 				</div>
@@ -50,7 +59,7 @@
 				<a href="/timeline/{topic}/{datapoint.Jahr}">
 					<circle
 						cx={x(datapoint.Jahr)}
-						cy={datapoint.Ort === 'Biel' ? y(topic) : y(i === 0 ? 'Up' : 'Down')}
+						cy={(datapoint.Ort === 'Biel' ? y(topic) : y(i === 0 ? 'Up' : 'Down')) || 0}
 						r="5"
 						fill={colors[i]}
 					/>
