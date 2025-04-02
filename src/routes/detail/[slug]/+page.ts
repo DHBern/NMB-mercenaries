5e055cd5eb21892abec16193a60e1bceda97dfbb
flicksolutions <sebastian@flick.solutions>
2025-04-02 17:39:28 +0200

    prepare detail

---
+++ src/routes/detail/[slug]/+page.ts
@@ -0,0 +1,35 @@
+import type { PageLoad } from './$types';
+import anderswo from '$lib/data/anderswo.json';
+import imgInfo from '$lib/data/img_info.json';
+
+export const load = (async ({ params }) => {
+	const { slug } = params;
+	const [yearString, topic] = slug.split('_');
+	const year = Number(yearString);
+	const anderswoData = anderswo.find((item) => item.Jahr === year && item.Titel === topic);
+	if (anderswoData) {
+		return {
+			type: 'anderswo',
+			year,
+			topic,
+			content: anderswoData
+		};
+	} else {
+		const imgData = imgInfo.info.find((item) => item.Bild === slug);
+		if (imgData?.Metatext) {
+			imgData.Metatext = imgInfo.metatext[imgData.Metatext - 1];
+		}
+		if (imgData) {
+			return {
+				type: 'img',
+				year,
+				topic,
+				content: imgData
+			};
+		} else {
+			return {
+				type: 'not-found'
+			};
+		}
+	}
+}) satisfies PageLoad;
