01a20b5c7a2e4c291a4199b689910355d0ecdfdb
vvvyyynet <levyn.buerki@unibe.ch>
2025-04-02 13:41:38 +0200

    map: add overview map

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { PageData } from '../$types';
+	import Map_full from './Map_full.svelte';
 
 	let { data }: { data: PageData } = $props();
+
 </script>
+
+<Map_full />
+
+<style>
+	img.hidden {
+		display:none;
+	}
+	:root {
+		--state: 3; /* 1 = java, 2 = rivers, 3 = europe */
+	}
+</style>
