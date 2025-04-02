e2cfcb2f664aee646addd54e8a3536952f949621
flicksolutions <sebastian@flick.solutions>
2025-04-02 19:15:56 +0200

    style header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import { locales, setLocale } from '$lib/paraglide/runtime';
 
 	let { children } = $props();
 </script>
 
-<header><button>intro</button><button>language selectors</button></header>
+<header class="flex justify-between gap-4 p-4">
+	<a class="btn btn-lg preset-filled-primary-500" href="/intro/1">Zum Start</a>
+	<div class="flex gap-4">
+		{#each locales as locale}
+			<button class="btn btn-lg preset-filled-primary-500" onclick={() => setLocale(locale)}>
+				{locale}
+			</button>
+		{/each}
+	</div>
+</header>
 {@render children()}
