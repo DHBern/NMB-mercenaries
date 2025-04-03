a3dfa663d1261750604c169c582290dfb4bda33f
vvvyyynet <levyn.buerki@unibe.ch>
2025-04-03 11:50:07 +0200

    style: dark layouts for map and detail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,14 @@
 	import '../app.css';
 	import { locales, setLocale } from '$lib/paraglide/runtime';
 
+	let isMap = true;
+	let isDetail = false;
+
 	let { children } = $props();
 </script>
 
 <div class="grid-rows-[1fr_11fr] grid h-screen max-h-screen">
-	<header class="flex justify-between gap-4 p-4">
+	<header class={["flex justify-between gap-4 p-4", (isDetail || isMap) &&'bg-surface-400']}>
 		<a class="btn btn-lg preset-filled-primary-500 h-16" href="/intro/1">Zum Start</a>
 		<div class="flex gap-4">
 			{#each locales as locale}
