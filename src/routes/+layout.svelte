ead627a2dab85a863a8cb6b30f8143f243435f39
vvvyyynet <levyn.buerki@unibe.ch>
2025-04-03 12:03:54 +0200

    fix: make header color change color on route change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,16 @@
 	import '../app.css';
 	import { locales, setLocale } from '$lib/paraglide/runtime';
 
-	let isMap = true;
-	let isDetail = false;
+	import { page } from '$app/state';
+
+	let isDark = $derived(page.url.pathname.includes('/map') || page.url.pathname.includes('/detail') ? true : false)
+	console.log(isDark);
 
 	let { children } = $props();
 </script>
-
+<p>{page.url.pathname}</p>
 <div class="grid-rows-[1fr_11fr] grid h-screen max-h-screen">
-	<header class={["flex justify-between gap-4 p-4", (isDetail || isMap) &&'bg-surface-400']}>
+	<header class={["flex justify-between gap-4 p-4", (isDark) &&'bg-surface-400']}>
 		<a class="btn btn-lg preset-filled-primary-500 h-16" href="/intro/1">Zum Start</a>
 		<div class="flex gap-4">
 			{#each locales as locale}
