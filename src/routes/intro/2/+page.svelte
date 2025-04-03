8297e2391c3d1296fee9cb8569d9cc02e6351a2c
vvvyyynet <levyn.buerki@unibe.ch>
2025-04-03 09:35:59 +0200

    feat (intro): refactor text paragraphs

--- src/routes/intro/2/+page.svelte
+++ src/routes/intro/2/+page.svelte
@@ -7,10 +7,13 @@
 <div class="mx-auto max-w-2xl">
 	<p class="text-2xl">
 		Das Leben der Menschen ist gekennzeichnet durch Möglichkeiten und Entscheide. Bei Vertretern der
-		Bieler Oberschicht waren diese grösser, als bei Menschen tieferer Schichten. Georg Friedrich
-		Heilmann und Francois Emil Neuhaus entschieden sich für Militärkarrieren im Ausland. Diese
-		bietet den beiden Vertreter der Bieler Oberschicht die Möglichkeit, die Welt zu sehen, lukrative
-		Pensionen, den Aufbau von weltweiten Netzwerken und nicht zuletzt auch soziale Prestige. Doch
-		dieser Weg ist auch mit der tödlichen Gefahr durch Kampfhandlungen und Krankheiten verbunden.
+		Bieler Oberschicht waren diese grösser, als bei Menschen tieferer Schichten.
+	</p>
+	<p class="text-2xl">
+		Georg Friedrich Heilmann und Francois Emil Neuhaus entschieden sich für Militärkarrieren im
+		Ausland. Diese bietet den beiden Vertreter der Bieler Oberschicht die Möglichkeit, die Welt zu
+		sehen, lukrative Pensionen, den Aufbau von weltweiten Netzwerken und nicht zuletzt auch soziale
+		Prestige. Doch dieser Weg ist auch mit der tödlichen Gefahr durch Kampfhandlungen und
+		Krankheiten verbunden.
 	</p>
 </div>
--- src/routes/intro/3/+page.svelte
+++ src/routes/intro/3/+page.svelte
@@ -6,18 +6,22 @@
 </script>
 
 <div class="mx-auto max-w-2xl">
-	<p class="text-2xl">
-		Entscheide dich für einen der beiden Bieler Söldner und erfahre, mehr über die Folgen seiner
-		Entscheide. Doch was wäre, wenn man in Biel geblieben wäre? Was kann uns der Vennerbrunnen in
-		der Altstadt erzählen? Du kannst jederzeit zwischen Zeit, Mensch und Ort wechseln. Denn es gibt
+	<p class="text-2xl pt-5">
+		Entscheide dich für einen der beiden Bieler Söldner und erfahre mehr über die Folgen seiner
+		Entscheide.
+	</p>
+	<a class={["text-xl ml-50 my-5 btn btn-lg", colors['preset-filled']['Heilmann']]} href="../timeline/Heilmann/1785">Ich folge Georg Friedrich Heilmann</a>
+	<a class={["text-xl ml-10 my-5 btn btn-lg", colors['preset-filled']['Neuhaus']]} href="../timeline/Neuhaus/1794">Ich folge François Emile Neuhaus</a>
+	<p class="text-2xl pt-5">
+		Doch was wäre, wenn man in Biel geblieben wäre? Was kann uns der Vennerbrunnen in
+		der Altstadt erzählen? 
+	</p>
+	<div class="pt-10">
+		<a class={["text-xl ml-40 my-5 btn btn-lg", colors['preset-filled']['Brunnen']]} href="../timeline/Brunnen/1798">Ich bleibe in Biel</a>
+	</div>
+	<p class="text-2xl pt-5">
+		Du kannst jederzeit zwischen Zeit, Mensch und Ort wechseln. Denn es gibt
 		nicht den vorgegebenen Lebenslauf, sondern Lebensrealitäten, die sich je nach Ort und
 		getroffenem Entscheid voneinander unterscheiden.
 	</p>
-	<div class="pt-10">
-		<a class={["text-xl ml-50 my-5 inline-block btn btn-lg", colors['preset-filled']['Heilmann']]} href="../timeline/Heilmann/1785">Biografie von Georg Friedrich Heilmann</a>
-		<br />
-		<a class={["text-xl ml-10 my-5 inline-block btn btn-lg", colors['preset-filled']['Neuhaus']]} href="../timeline/Neuhaus/1794">Biografie von François Emile Neuhaus</a>
-		<br />
-		<a class={["text-xl ml-40 my-5 inline-block btn btn-lg", colors['preset-filled']['Brunnen']]} href="../timeline/Brunnen/1798">Ich bleibe in Biel</a>
-	</div>
 </div>
