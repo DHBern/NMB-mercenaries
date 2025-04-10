994aa3cb55df2fa1813858ac22f3abbbc75e4c93
Sebi <sebastian@flick.solutions>
2025-04-10 16:01:47 +0200

    create onIdle function and Modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,17 @@
 	import { locales, setLocale } from '$lib/paraglide/runtime';
 	import { page } from '$app/state';
 	import { base } from '$app/paths';
+	import { listen, onIdle } from 'svelte-idle';
+	import { goto } from '$app/navigation';
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
+
+	let { children } = $props();
+
+	listen({
+		// timer: 5000, //uncomment to test the modal
+		timer: 60_000 * 3, // 3 minutes
+		cycle: 1500
+	});
 
 	function handleGoBack() {
 		history.back();
@@ -14,8 +25,54 @@
 	let isMap = $derived(page.url.pathname.includes('/map') ? true : false);
 	let isDetail = $derived(page.url.pathname.includes('/detail') ? true : false);
 
-	let { children } = $props();
+	let openState = $state(false);
+	let remaining = $state(10);
+	let timerToHome: number | undefined = undefined;
+
+	function modalClose() {
+		openState = false;
+		clearInterval(remaining);
+		clearTimeout(timerToHome);
+	}
+
+	onIdle(() => {
+		if (!page.url.pathname.includes('/intro/1')) {
+			openState = true;
+			setInterval(() => {
+				remaining -= 1;
+			}, 1000);
+			timerToHome = setTimeout(() => {
+				openState = false;
+				clearInterval(remaining);
+				goto(base + '/intro/1', { replaceState: true });
+			}, 10000);
+		}
+	});
 </script>
+
+<Modal
+	open={openState}
+	onOpenChange={(e) => (openState = e.open)}
+	triggerBase="btn preset-tonal"
+	contentBase="card bg-surface-100-900 p-4 space-y-4 shadow-xl max-w-screen-sm"
+	backdropClasses="backdrop-blur-sm"
+>
+	{#snippet content()}
+		<header class="flex justify-between">
+			<h2 class="h2">Sind Sie noch da?</h2>
+		</header>
+		<article>
+			<p class="opacity-60">
+				Die Applikation wird in {remaining} Sekunden zurückgesetzt, wenn Sie nicht reagieren.
+			</p>
+		</article>
+		<footer class="flex justify-around gap-4">
+			<button type="button" class="btn btn-lg preset-filled" onclick={modalClose}
+				>Ich bin noch da!</button
+			>
+		</footer>
+	{/snippet}
+</Modal>
 
 <div
 	class={[
