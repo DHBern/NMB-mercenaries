945cfd57d2cd3f53508d9862b47ece40c478fabd
vvvyyynet <levyn.buerki@unibe.ch>
2025-04-14 13:48:46 +0200

    chore: remove commented button in header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,12 +26,6 @@
 		<a class="btn btn-lg preset-outlined-primary-500 h-12 font-semibold" href="/intro/1"
 			>Zum Start</a
 		>
-		<!-- {#if isMap }
-			<button
-				class="btn btn-lg preset-outlined-primary-500 h-12 font-semibold"
-				onclick={handleGoBack}>Zurück</button
-			>
-		{/if} -->
 		<div class="flex gap-4">
 			{#each locales as locale}
 				<button
