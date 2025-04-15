694f60b50ac75feab0b333190c063e3c8009c8c3
vvvyyynet <levyn.buerki@unibe.ch>
2025-04-15 19:30:59 +0200

    fix: make ping more visible

--- src/lib/components/Ping.svelte
+++ src/lib/components/Ping.svelte
@@ -2,17 +2,20 @@
 	let { classes, setWhite = false } = $props();
 	let color;
 	if (setWhite) {
-		color = ['bg-white', 'bg-white', 'bg-white', 'border-white'];
+		color = ['bg-white', 'bg-white', 'bg-white border-white', 'bg-white border-white'];
 	} else {
-		color = ['bg-surface-100', 'bg-surface-900', 'bg-white', 'border-surface-900'];
+		color = ['bg-surface-400', 'bg-surface-900', 'bg-white', 'border-surface-900'];
 	}
 </script>
 
-<span class={[classes, 'opacity-75 z-100']}>
-	<span class={[color, 'absolute inline-flex h-full w-full animate-ping rounded-full']}>
-		<span class={[color, 'absolute inline-flex h-full w-full animate-ping rounded-full']}>
-			<span class={[color, 'absolute inline-flex h-full w-full animate-ping rounded-full']}> </span>
-			<span class={[color, 'absolute inline-flex h-full w-full animate-ping rounded-full border']}>
+<span class={[classes, 'z-100 opacity-100']}>
+	<span class={[color[0], 'absolute inline-flex h-full w-full animate-ping rounded-full']}>
+		<span class={[color[1], 'absolute inline-flex h-full w-full animate-ping rounded-full']}>
+			<span class={[color[2], 'absolute inline-flex h-full w-full animate-ping rounded-full border']}>
+			</span>
+			<span
+				class={[color[3], 'absolute inline-flex h-full w-full animate-ping rounded-full border']}
+			>
 			</span>
 		</span>
 	</span>
