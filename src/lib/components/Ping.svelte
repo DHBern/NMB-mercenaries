9823a7e2ad6a245de63aec7314e1741ad93398ca
vvvyyynet <levyn.buerki@unibe.ch>
2025-04-15 11:01:41 +0200

    feat: create Ping component

---
+++ src/lib/components/Ping.svelte
@@ -0,0 +1,16 @@
+<script>
+	let { classes } = $props();
+</script>
+
+<span class={classes}>
+	<span class="bg-surface-500 absolute inline-flex h-full w-full animate-ping rounded-full">
+		<span class="bg-surface-900 absolute inline-flex h-full w-full animate-ping rounded-full">
+			<span class="bg-surface-100 absolute inline-flex h-full w-full animate-ping rounded-full">
+			</span>
+			<span
+				class="border-surface-900 absolute inline-flex h-full w-full animate-ping rounded-full border"
+			>
+			</span>
+		</span>
+	</span>
+</span>
