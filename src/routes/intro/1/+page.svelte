d899217640d1d326e09ea750b395ad0b039c5bf8
vvvyyynet <levyn.buerki@unibe.ch>
2025-08-11 11:19:12 +0200

    fix: reset paraglide

--- src/routes/intro/1/+page.svelte
+++ src/routes/intro/1/+page.svelte
@@ -1,1 +1,5 @@
-<h1 class="h1 absolute mx-auto text-6xl">Verflochtene Leben</h1>
+<script>
+	import { m } from '$lib/paraglide/messages';
+</script>
+
+<h1 class="h1 absolute mx-auto text-6xl">{m.awake_only_mammoth_assure()}</h1>
