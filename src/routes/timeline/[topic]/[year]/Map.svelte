06f857a01bba5bc30f53649a693214da3cbd0515
flicksolutions <sebastian@flick.solutions>
2025-08-13 23:01:06 +0200

    place svg on canvas

--- src/routes/timeline/[topic]/[year]/Map.svelte
+++ src/routes/timeline/[topic]/[year]/Map.svelte
@@ -56,9 +56,9 @@
 	});
 </script>
 
-<Canvas {rValues} {region} {topic} classes={['map', classes]} />
+<Canvas {rValues} {region} {topic} classes="map" />
 
-<svg class={['map', classes]} viewBox="0 0 1000 840" data-topic={topic}>
+<svg class={['map', 'absolute top-0', classes]} viewBox="0 0 1000 840" data-topic={topic}>
 	<!-- Highlighted circles -->
 	{#if region === 'europe'}
 		<circle
@@ -67,37 +67,84 @@
 			cx="905"
 			cy="185"
 			r="4.3961"
-			style="transform-origin: 905px 185px;"
 		/>
 		<circle
-			id="europe-baryssau-ping"
-			class={['marked europe-baryssau circle-18-90', 'origin-center animate-ping opacity-100']}
-			cx="905"
-			cy="185"
-			r="4.3961"
-			style="transform-origin: 905px 185px;"
+			id="europe-rotterdam"
+			class="marked europe-rotterdam circle-27-36"
+			cx="365"
+			cy="275"
+			r="3.4039"
 		/>
-	{:else}{/if}
+		<circle
+			id="europe-hertogenbosch"
+			class="marked europe-hertogenbosch circle-28-37"
+			cx="375"
+			cy="285"
+			r="4.2667"
+		/>
+		<circle
+			id="europe-halle"
+			class="marked europe-halle circle-28-55"
+			cx="555"
+			cy="285"
+			r="4.3529"
+		/>
+		<circle
+			id="europe-hulst"
+			class="marked europe-hulst circle-29-35"
+			cx="355"
+			cy="295"
+			r="4.1804"
+		/>
+		<circle
+			id="europe-heidelberg"
+			class="marked europe-heidelberg circle-35-47"
+			cx="475"
+			cy="355"
+			r="4.3961"
+		/>
+		<circle id="europe-wien" class="marked europe-wien circle-40-66" cx="665" cy="405" r="4.3529" />
+		<circle
+			id="europe-franche"
+			class="marked europe-franche circle-43-39"
+			cx="395"
+			cy="435"
+			r="4.3961"
+		/>
+		<circle id="europe-biel" class="marked europe-biel circle-43-44" cx="445" cy="435" r="4.3961" />
+		<circle
+			id="europe-neapel"
+			class="marked europe-neapel circle-62-59"
+			cx="595"
+			cy="625"
+			r="4.1373"
+		/>
+	{:else}
+		<circle
+			id="seasia-banjarmasin"
+			class="circle-46-70 marked seasia-banjarmasin"
+			cx="705"
+			cy="465"
+			r="4.3961"
+		/>
+		<circle
+			id="seasia-padangse"
+			class="circle-56-40 marked seasia-padangse"
+			cx="405"
+			cy="565"
+			r="3.9647"
+		/>
+		<circle
+			id="seasia-batavia"
+			class="circle-67-54 marked seasia-batavia"
+			cx="545"
+			cy="675"
+			r="4.3529"
+		/>
+	{/if}
 </svg>
 
 <style>
-	@keyframes pulse {
-		0% {
-			r: 20;
-			fill: 'red';
-			opacity: 1;
-		}
-		50% {
-			r: 50;
-			fill: 'blue';
-			opacity: 0.6;
-		}
-		100% {
-			r: 20;
-			fill: 'red';
-			opacity: 1;
-		}
-	}
 	:global(.bigger) {
 		r: 12px;
 	}
