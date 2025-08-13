4c9bac9f9225148c25a4337aec3be0fa81afd278
flicksolutions <sebastian@flick.solutions>
2025-08-14 00:06:25 +0200

    fix path

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import { base } from '$app/paths';
 
 export const load = (async () => {
-	redirect(307, '/intro/1');
+	redirect(307, `${base}/intro/1`);
 }) satisfies PageLoad;
