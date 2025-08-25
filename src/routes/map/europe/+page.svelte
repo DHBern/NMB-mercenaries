c4ea512f4f4a41bf8ee8649d46e71aab355eb188
vvvyyynet <levyn.buerki@unibe.ch>
2025-08-25 12:59:43 +0200

    feat: add french map labels

--- src/routes/map/europe/+page.svelte
+++ src/routes/map/europe/+page.svelte
@@ -2,12 +2,13 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
-	// import CirclesEurope from '$lib/circle-components/Circles-europe.svelte';
+	import { m } from '$lib/paraglide/messages.js';
 	import CirclesEurope from '$lib/circle-components/Circles-europe-150.svelte';
 	import { colors } from '$lib/metadata.json';
 	import FullMap from '../FullMap.svelte';
 	import { onNavigate } from '$app/navigation';
 	import { localizeHref } from '$lib/paraglide/runtime';
+	import { page } from '$app/state';
 
 	let isPulsating = $state(false);
 	let timerPing = setTimeout(() => {
@@ -93,61 +94,71 @@
 		<foreignObject x="625" y="415" width="150" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Biel/1798`)}
-				class={['btn font-semibold', colors['preset-filled']['Biel']]}>Biel, Hofwil</a
+				class={['btn font-semibold', colors['preset-filled']['Biel']]}
+				>{m.spicy_equal_sparrow_bake()}</a
 			>
 		</foreignObject>
 		<foreignObject x="775" y="570" width="150" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Heilmann/1829`)}
-				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}>Neapel, Nola</a
+				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}
+				>{m.awful_major_shell_cut()}</a
 			>
 		</foreignObject>
 		<foreignObject x="625" y="325" width="150" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Heilmann/1804`)}
-				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}>Heidelberg</a
+				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}
+				>{m.lower_inner_shrike_build()}</a
 			>
 		</foreignObject>
 		<foreignObject x="840" y="395" width="100" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Heilmann/1814`)}
-				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}>Wien</a
+				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}
+				>{m.stock_witty_sawfish_devour()}</a
 			>
 		</foreignObject>
-		<foreignObject x="735" y="265" width="100" height="50">
+		<foreignObject x="735" y="265" width="150" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Heilmann/1829`)}
-				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}>Halle</a
+				class={['btn font-semibold', colors['preset-filled']['Heilmann']]}
+				>{m.wide_next_marmot_value()}</a
 			>
 		</foreignObject>
 		<foreignObject x="1115" y="165" width="100" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Neuhaus/1812`)}
-				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}>Baryssau</a
+				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}
+				>{m.patient_pink_nuthatch_splash()}</a
 			>
 		</foreignObject>
 		<foreignObject x="415" y="415" width="185" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Neuhaus/1815`)}
-				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}>Franche-Comté</a
+				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}
+				>{m.candid_many_shell_dare()}</a
 			>
 		</foreignObject>
 		<foreignObject x="545" y="265" width="180" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Neuhaus/1816`)}
-				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}>‘s-Hertogenbosch</a
+				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}
+				>{m.bright_inner_pigeon_link()}</a
 			>
 		</foreignObject>
 		<foreignObject x="440" y="205" width="100" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Neuhaus/1831`)}
-				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}>Hulst</a
+				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}
+				>{m.full_muddy_ostrich_leap()}</a
 			>
 		</foreignObject>
 		<foreignObject x="555" y="195" width="150" height="50">
 			<a
 				href={localizeHref(`${base}/timeline/Neuhaus/1843`)}
-				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}>Rotterdam</a
+				class={['btn font-semibold', colors['preset-filled']['Neuhaus']]}
+				>{m.fair_zany_mantis_favor()}</a
 			>
 		</foreignObject>
 		<!-- Pull marked circles to front -->
