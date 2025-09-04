71c5c40ad06e49663040c4eb0bb7a19437d6ce5f
vvvyyynet <levyn.buerki@unibe.ch>
2025-09-04 08:22:13 +0200

    reorganise intro 3

--- src/routes/intro/3/+page.svelte
+++ src/routes/intro/3/+page.svelte
@@ -11,38 +11,51 @@
 <div class="mx-auto w-[80%] pt-30">
 	<p class="mb-30 text-4xl">{m.that_just_kangaroo_flop()}</p>
 
-	<div class="flex-cols-3 my-5 mb-15 flex justify-around gap-10">
-		<div class="flex flex-col items-center gap-10">
+	<div class="flex-cols-3 my-5 mb-15 flex justify-around gap-10 pt-10 pl-50">
+		<div class="relative translate-y-[0px]">
 			<a href="../timeline/Heilmann/1785"
-				><img class="max-h-120 max-w-120 opacity-70" src={portrait_heilmann} alt="heilmann" /></a
+				><img class="max-w-100 opacity-70" src={portrait_heilmann} alt="heilmann" /></a
 			>
 			<a
-				class={['btn btn-lg max-h-15 w-100 text-xl', colors['preset-filled']['Heilmann']]}
+				class={[
+					'btn absolute -bottom-10 -left-10 h-10 max-h-15 min-h-20 w-120 -rotate-1 p-8 text-2xl',
+					colors['preset-filled']['Heilmann']
+				]}
 				href="../timeline/Heilmann/1785"
 			>
-				<ArrowRight class="mr-3" />
+				<ArrowRight class="mr-3" size="40" />
 				<span>Georg Friedrich Heilmann</span>
 			</a>
 		</div>
-		<div class="flex flex-col items-center gap-10">
-			<a href="../timeline/Neuhaus/1794"
-				><img class="max-h-120 max-w-120 opacity-70" src={portrait_neuhaus} alt="neuhaus" /></a
-			>
-			<a
-				class={['btn btn-lg max-h-15 w-100 text-xl', colors['preset-filled']['Neuhaus']]}
-				href="../timeline/Neuhaus/1794"
-			>
-				<ArrowRight class="mr-3" /><span>François Emile Neuhaus</span></a
-			>
-		</div>
-		<div class="flex flex-col items-center gap-10">
+		<div class="relative translate-y-[140px]">
 			<a href="../timeline/Biel/1798">
-				<img class="max-h-120 max-w-120 opacity-70" src={portrait_brunnen} alt="brunnen" />
+				<img
+					class="max-w-100 translate-y-[140px] opacity-70"
+					src={portrait_brunnen}
+					alt="brunnen"
+				/>
 			</a>
 			<a
-				class={['btn btn-lg max-h-15 w-100 text-xl', colors['preset-filled']['Biel']]}
+				class={[
+					'btn absolute -bottom-10 -left-10 max-h-15 min-h-20 w-120 -rotate-1 p-8 text-2xl',
+					colors['preset-filled']['Biel']
+				]}
 				href="../timeline/Biel/1798"
-				><ArrowRight class="mr-3" /><span>{m.jolly_proud_nils_pop()}</span></a
+				><ArrowRight class="mr-3" size="40" /><span>{m.jolly_proud_nils_pop()}</span></a
+			>
+		</div>
+		<div class="relative translate-x-[20px] translate-y-[60px]">
+			<a href="../timeline/Neuhaus/1794"
+				><img class="max-w-100 opacity-70" src={portrait_neuhaus} alt="neuhaus" /></a
+			>
+			<a
+				class={[
+					'btn absolute -bottom-6 -left-10 max-h-15 min-h-20 w-120 -rotate-1 p-8 text-2xl',
+					colors['preset-filled']['Neuhaus']
+				]}
+				href="../timeline/Neuhaus/1794"
+			>
+				<ArrowRight class="mr-3" size="40" /><span>François Emile Neuhaus</span></a
 			>
 		</div>
 	</div>
